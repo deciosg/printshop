261f8b60e63be46a48acb8df7118002e61150491
Decio Silva Gomes <deciosg1@gmail.com>
2016-06-24 16:40:07 -0300

    Classes de inicialização do Spring Boot

---
+++ src/main/java/cc/printshop/PrintShopApplication.java
@@ -0,0 +1,12 @@
+package cc.printshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintShopApplication.class, args);
+	}
+}
